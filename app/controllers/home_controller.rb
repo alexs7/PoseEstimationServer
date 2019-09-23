061408093e10088b729647b91488591a3efbf0a2
alex <alexandrosrotsides@gmail.com>
2019-09-23 10:12:07 +0100

    returning more data to the phone

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -12,12 +12,13 @@ class HomeController < ApplicationController
     File.open("arcore.JPG", 'w') { |file| file.write(byte_array.force_encoding('UTF-8'))}
 
     # run the script here
-    system("./get_pose_server_script.sh")
+    system("./get_pose_server_script.sh") # pass the data dir here
     points3D = File.read("../../Python/results/arcore/3D_points_direct.txt")
     colmap_pose_translation = File.read("../../Python/results/arcore/pnp_ransac_translation_vector_direct.txt")
     colmap_pose_rotation_as_quat = File.read("../../Python/results/arcore/rotation_direct_matching_as_quaternion.txt")
+    points2d = File.read("../../Python/results/arcore/points2D_projected_direct_matching.txt")
 
-    data = points3D + colmap_pose_rotation_as_quat + colmap_pose_translation
+    data = points3D + points2d + colmap_pose_rotation_as_quat + colmap_pose_translation
 
     render json: data
   end
--- get_pose_server_script.sh
+++ get_pose_server_script.sh
@@ -1,3 +1,3 @@
-mv arcore.JPG ../../Python/data/work_lego_sets_corner/query_images/
+mv arcore.JPG ../../Python/data/uni_lego_scene/query_images/
 cd ../../Python/
-python2.7 get_pose.py 0 0 0 arcore.JPG data/work_lego_sets_corner intrinsics_matrices/pixel_intrinsics_low_640.txt 0 0
+python2.7 get_pose.py 0 0 0 arcore.JPG data/uni_lego_scene intrinsics_matrices/pixel_intrinsics_low_640.txt 0 0
